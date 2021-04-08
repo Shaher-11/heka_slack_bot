dc5d21d5f49bfc2a4d6abdf5e17dbe4a8c608054
Shaher-11 <shahershamroukh@gmail.com>
2021-04-08 14:17:33 +0200

    Set the help method and command

--- heka_ruby/bot.rb
+++ heka_ruby/bot.rb
@@ -1,5 +1,14 @@
 module HekaRuby
   class Bot < SlackRubyBot::Bot
-    
+    help do 
+      title 'Heka Ruby Bot'
+      desc 'This is bot about Ruby vedios'
+
+      command :get_latest_info do
+        title 'get_latest_info'
+        desc 'Returns the url of th latest ruby vedios'
+        long_desc 'This is a longggggggggggggggggggggg descriptioonnnnnnnnnnnnnnnn'
+      end
+    end
   end
 end
