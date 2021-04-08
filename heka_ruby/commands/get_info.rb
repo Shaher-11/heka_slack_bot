a61c17204794fbbd333b71af3af5cd090e9b7b1c
Shaher-11 <shahershamroukh@gmail.com>
2021-04-08 12:33:01 +0200

    Add command method for links

--- heka_ruby/commands/get_info.rb
+++ heka_ruby/commands/get_info.rb
@@ -3,7 +3,12 @@ require 'open-uri'
 
 module HekaRuby
   module Commands
-    
+    class GetInfo < SlackRubyBot::Commands::Base
+      command 'get_some_info' do |client, data, _match|
+        url = 'https://example.com'
+        rss = RSS:Parser.parse(open(url).read, false).items.first
+        client.say(channel: data.channel, text: rss.link)
+      end
     end
   end
 end
